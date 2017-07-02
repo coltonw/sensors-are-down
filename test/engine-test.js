f8b567ad883b9915b2675614ebaf45d09a498636
Will Colton <coltonw@gmail.com>
2017-07-02 12:39:58 -0400

    Add a unit test and fix a bunch of bugs.

--- test/engine-test.js
+++ test/engine-test.js
@@ -6,7 +6,6 @@ describe('engine', () => {
   it('should initialize properly', () => {
     const store = engine.init(null);
     store.dispatch(engine.startGame());
-    console.dir(store.getState().game.offenseCardChoices);
     const choiceNames = _.map(
       _.values(store.getState().game.offenseCardChoices.playerCards), value => value.name);
     assert.equal(choiceNames.length, 2, 'there are two choices');
@@ -65,5 +64,22 @@ describe('engine', () => {
       playerCards: [{ strength: 2, someIgnored: 'stuff' }],
       aiCards: [{ strength: 1, defense: 2 }],
     }, 'defense on cards works properly');
+  });
+
+  it('should properly play cards', () => {
+    const store = engine.init(null);
+    store.dispatch(engine.startGame());
+    const firstCardId = Object.keys(store.getState().game.offenseCardChoices.playerCards)[0];
+    const firstCard = store.getState().game.offenseCardChoices.playerCards[firstCardId];
+    assert.equal(Object.keys(store.getState().game.offenseCardChoices.playerCards).length, 2, 'two choices are presented');
+    store.dispatch(engine.pickOffenseCard(firstCardId));
+    assert(store.getState().game.offenseCardChoices.playerCards[firstCardId], 'the selected card is still in the choices');
+    assert.equal(Object.keys(store.getState().game.offenseCardChoices.playerCards).length, 1, 'the selected card is the only one left');
+    const allPlayerCardsInPlay = [
+      ...store.getState().game.planet.playerCards,
+      ...store.getState().game.ships.playerShip.playerCards,
+      ...store.getState().game.ships.aiShip.playerCards];
+    assert.equal(allPlayerCardsInPlay.length, 1, 'there should be only one player card in play');
+    assert.deepEqual(allPlayerCardsInPlay, [_.assign({}, firstCard, { cardId: firstCardId })], 'the card played should be in play');
   });
 });
